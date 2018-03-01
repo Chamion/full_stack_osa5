5076f3dffe2ab002cbee1f6ddbee3e822ea5d443
Chamion <jemi.salo@gmail.com>
2018-03-01 19:49:37 +0200

    teht2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -8,12 +8,18 @@ import loginService from './services/login'
 class App extends React.Component {
     constructor(props) {
         super(props)
+        var user
+        try {
+            user = JSON.parse(localStorage.getItem('user'))
+        } catch(err) {
+            user = null
+        }
         this.state = {
             blogs: [],
             username: '',
             password: '',
             error: null,
-            user: null
+            user
         }
     }
 
@@ -35,7 +41,7 @@ class App extends React.Component {
                 password: '',
                 user
             })
-            console.log(user)
+            localStorage.setItem('user', JSON.stringify(user))
         } catch(err) {
             this.setState({
                 error: 'käyttäjätunnus tai salasana virheellinen',
@@ -48,6 +54,7 @@ class App extends React.Component {
     
     handleLogout = (event) => {
         event.preventDefault()
+        localStorage.removeItem('user')
         this.setState({
             user: null
         })
@@ -58,7 +65,21 @@ class App extends React.Component {
     }
 
     render() {
-        if(this.state.user === null) {
+        if(this.state.user) {
+            return (
+                <div>
+                    <LogoutForm username={this.state.user.username} logoutHandler={this.handleLogout.bind(this)}/>
+                    <h2>blogs</h2>
+                    <table>
+                        <tbody>
+                            {this.state.blogs.map(blog => 
+                                <Blog key={blog._id} blog={blog}/>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            )
+        } else {
             return (
                 <div>
                     <LoginForm loginHandler={this.handleLogin.bind(this)} 
@@ -70,19 +91,6 @@ class App extends React.Component {
                 </div>
             )
         }
-        return (
-            <div>
-                <LogoutForm username={this.state.user.username} logoutHandler={this.handleLogout.bind(this)}/>
-                <h2>blogs</h2>
-                <table>
-                    <tbody>
-                        {this.state.blogs.map(blog => 
-                            <Blog key={blog._id} blog={blog}/>
-                        )}
-                    </tbody>
-                </table>
-            </div>
-        );
     }
 }
 
